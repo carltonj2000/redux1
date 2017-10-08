3dbc2c53b4c0b92ed249f7522712742d249ba375
Carlton Joseph <carlton.joseph@gmail.com>
2017-10-07 22:31:22 -0700

    Add multiple counter support

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,35 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { incrementCounter, decrementCounter } from '../actions';
 
 class Counter extends Component {
-  // state = {
-  //   count: 7,
-  // };
-
-  // increment = () => this.setState(state => ({...state, count: state.count + 1}));
-  // decrement = () => this.setState(state => ({...state, count: state.count - 1}));
-
   render() {
     return (
-      <div className="Counter">
-        Counter: {this.props.counter.count}<br />
+      <span className="Counter">
+        Counter : {this.props.count}&nbsp;
         <button onClick={this.props.increment}>+</button>
         <button onClick={this.props.decrement}>-</button>
-      </div>
+      </span>
     );
   }
 }
-const mapStateToProps = ({counter}) => {
-  return { counter: counter };
-}
 
-const mapDispatchToProps = dispatch => {
-  return {
-    decrement: () => dispatch(decrementCounter()),
-    increment: () => dispatch(incrementCounter()),
-  }
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
---
+++ src/components/Counters.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  addCounter,
+  removeCounter,
+  incrementCounter,
+  decrementCounter
+} from '../actions';
+import Counter from './Counter';
+
+class Counters extends Component {
+  render() {
+    return (
+      <span className="Counters">
+        {this.props.counters.counters.map((counter, index) =>
+          <div key={index}>
+          <span>{index}</span>&nbsp;
+          <Counter
+            increment={() => this.props.increment(index)}
+            decrement={() => this.props.decrement(index)}
+            count={counter}
+          />
+        <button onClick={() => this.props.remove(index)}>Remove Counter</button>
+        </div>
+        )}
+        <button onClick={this.props.add}>Add Counter</button>
+      </span>
+    );
+  }
+}
+
+const mapStateToProps = ({counters}) => {
+  return { counters: counters };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    add: () => dispatch(addCounter()),
+    remove: (index) => dispatch(removeCounter(index)),
+    increment: (index) => dispatch(incrementCounter(index)),
+    decrement: (index) => dispatch(decrementCounter(index))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counters);
